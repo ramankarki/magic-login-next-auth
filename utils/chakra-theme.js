cffb531e70d1d1c0249dc316e70443300dc9540c
ramankarki <ramankarki40@gmail.com>
2022-02-18 23:04:12 +0545

    dont use system color mode

--- utils/chakra-theme.js
+++ utils/chakra-theme.js
@@ -1,7 +1,7 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const config = {
-  useSystemColorMode: true,
+  useSystemColorMode: false,
   initialColorMode: 'dark', // this is fallback if system color is not resolved
 };
 
